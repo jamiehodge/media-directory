529cfe2f7e97a95da6bf68043c34e567f5410a9c
Jamie Hodge <jamiehodge@me.com>
2013-11-17 22:17:24 +0100

    fix /authenticate

--- lib/media/directory/controllers/people.rb
+++ lib/media/directory/controllers/people.rb
@@ -8,19 +8,18 @@ module Media
         set(:model)  { Models::Person }
         set(:policy) { Policies::Base }
 
-        get "/authenticate" do
-          @item = self.class.model.authenticate(parameters.id, parameters.password)
-
-          return 403 unless authorize.show?
-
-          respond_with :show, locals: { item: present(id) }
-        end
 
         index
         show
 
         def parameters
           self.class.parameters.new(params, pattern: /[a-z]{3}[1-9]{3}/)
+        post "/authenticate" do
+          @item = self.class.model.authenticate(parameters.id, parameters.password)
+
+          return 403 unless @item && authorize.show?
+
+          respond_with :show, locals: { item: present(item) }
         end
       end
     end
