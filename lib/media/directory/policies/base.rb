88aa0016e17fbbd11e6adbccfa83102c81420e90
Jamie Hodge <jamiehodge@me.com>
2013-11-17 22:16:28 +0100

    don't override #collection

--- lib/media/directory/policies/base.rb
+++ lib/media/directory/policies/base.rb
@@ -4,10 +4,6 @@ module Media
   module Directory
     module Policies
       class Base < Web::Policies::Static
-
-        def collection
-          self.class.collection(person, item.class)
-        end
       end
     end
   end
